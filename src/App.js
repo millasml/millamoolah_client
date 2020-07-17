b5cf9717ac56bf74c53791721b61f3acf6335c6e
millasml <millasml@yahoo.com.sg>
2020-07-17 22:24:31 +0800

    Dockerfile

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [apiResponse, setApiResponse] = useState("");
+
+  function callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then((res) => res.text())
+      .then((res) => setApiResponse( res ));
+  }
+
+  useEffect(() => {
+    callAPI();
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {apiResponse}
         </p>
         <a
           className="App-link"
